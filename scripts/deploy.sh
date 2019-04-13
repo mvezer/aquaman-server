d39d10f04232e9b4ce8918788884eb32303fb922
Matyas Vezer <vezer.m@gmail.com>
2019-04-13 16:29:32 +0200

    Update logging

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,1 @@
-sshpass -p aquaman scp -rpq package.json package-lock.json scripts src aquaman@192.168.178.116:/home/aquaman
+sshpass -p aquaman scp -rpq package.json package-lock.json schedules.json scripts src log aquaman@192.168.178.116:/home/aquaman
--- src/infrastructure/logger.js
+++ src/infrastructure/logger.js
@@ -1,8 +1,19 @@
 const SimpleLogger = require('simple-node-logger');
-const logManager = new SimpleLogger();
-logManager.createConsoleAppender();
-logManager.createFileAppender( { logFilePath:process.env.LOG_FILE || 'aquaman.log' } );
+
+const logDirectory = process.env.LOG_DIR || 'log';
+const createOpts = (domain, level) => {
+    return {
+        errorEventName:'error',
+            logDirectory,
+            fileNamePattern:'aquaman-<DATE>.log',
+            dateFormat:'YYYY.MM.DD',
+            domain,
+            level
+    };
+};
 
 module.exports = (loggerCategory, logLevel) => {
-    return logManager.createLogger(`[${loggerCategory}]`, logLevel || 'info');
+    const log = SimpleLogger.createRollingFileLogger(createOpts(`[${loggerCategory}]`, logLevel || 'info'));
+    log.addAppender(new SimpleLogger.appenders.ConsoleAppender());
+    return log;
 };
